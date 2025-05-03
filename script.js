7077c43fe959ab6fe93ef6c4726aff63bb38f7e0
keerthana-duraisamy <166682090+keerthana-duraisamy@users.noreply.github.com>
2025-05-03 19:37:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// Toggle icon navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navBar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("fa-xmark");
+    navBar.classList.toggle("active");
+};
+
+// Scroll section active link
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove("active");
+                document.querySelector(`header nav a[href*="${id}"]`).classList.add("active");
+            });
+        }
+    });
+
+    // Sticky navbar
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 100);
+
+    // Remove toggle icon and navbar
+    menuIcon.classList.remove("fa-xmark");
+    navBar.classList.remove("active");
+};
+
+// Scroll reveal animations
+ScrollReveal({
+    distance: "80px",
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(".home-img, .skills-content, .projects-box, .contact", { origin: "bottom" });
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".about-content,contact-content", { origin: "right" });
